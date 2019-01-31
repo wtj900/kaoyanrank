180ed88c29326768ab51846f3223513cadd8d46d
王史超 <wangshichao@offcn.com>
2019-01-31 21:00:39 +0800

    欢迎界面

---
+++ pages/welcome/welcome.js
@@ -0,0 +1,95 @@
+// pages/welcome/welcome.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    remind: '加载中',
+    angle: 0,
+    userInfo: {}
+  },
+
+  goToIndex: function () {
+    wx.switchTab({
+      url: '/pages/index/index',
+    });
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+    var _this = this;
+    setTimeout(function () {
+      _this.setData({
+        remind: ''
+      });
+    }, 1000);
+    wx.onAccelerometerChange(function (res) {
+      var angle = -(res.x * 30).toFixed(1);
+      if (angle > 14) { angle = 14; }
+      else if (angle < -14) { angle = -14; }
+      if (_this.data.angle !== angle) {
+        _this.setData({
+          angle: angle
+        });
+      }
+    });
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+    console.log('onLoad')
+    var that = this
+    app.getUserInfo(function (userInfo) {
+      that.setData({
+        userInfo: userInfo
+      })
+    })
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
